342aca07c4c3a29802a0c75d08a48f8c502085cd
iamvinchi <vincent@conglobal.co>
2024-07-23 10:26:30 +0100

    added environment check

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,3 +1,3 @@
-export const isDevEnvironment = true; 
+export const isDevEnvironment = false; 
 export const isStagingEnvironment = false;
-export const isProductionEnvironment = false;
+export const isProductionEnvironment = true;
